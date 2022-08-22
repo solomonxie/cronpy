9f48bef1a33166e867d8a91aaec5c63a7b54fb28
Solomon Xie <solomonxiewise@gmail.com>
2022-08-23 00:05:32 +0800

    fix datetime

--- cronpy/cron_parser_wip4.py
+++ cronpy/cron_parser_wip4.py
@@ -1,43 +1,24 @@
 import logging
-from itertools import product
 from datetime import datetime
-from dateutil.relativedelta import relativedelta
 
 from cronpy.datetime_utils import is_int
 from cronpy.datetime_utils import get_utc_now
 from cronpy.datetime_utils import date_to_time
 from cronpy.datetime_utils import get_n_days_of_month
+from cronpy.datetime_utils import how_many_weeks_of_month
+from cronpy.datetime_utils import get_nth_week_by_datetime
 
 logger = logging.getLogger(__name__)
 
 MINUTE, HOUR, DOM, DOW, NWEEK, MONTH, YEAR = 0, 1, 2, 3, 4, 5, 6
 UNIT_NAMES = ['MINUTE', 'HOUR', 'DOM', 'DOW', 'NWEEK', 'MONTH', 'YEAR']
-YYMM_TO_END_DAY = {
-    (yy, mm): get_n_days_of_month(yy, mm) for yy, mm in product(range(2000, 2100), range(1, 13))
-}
 OPT_MARKS = ('*', ',', '-', '/')
-
-
-def get_dom_by_nth_dow(year: int, month: int, dow: int, nth: int = None) -> int:
-    # FIXME: HOW TO LIMIT nth?
-    # ndays = YYMM_TO_END_DAY[(year, month)]
-    dt = datetime(year=year, month=month, day=1)
-    first_saturday = 7 - dt.isoweekday() % 7
-    nweek = nth + (0 if dow < dt.isoweekday() else -1)
-    target_saturday = first_saturday + nweek * 7
-    dom = target_saturday - (7 - dow) + 1
-    return dom
-
-
-def get_nth_week_by_datetime(dt: datetime) -> int:
-    this_saturday = dt.day - dt.isoweekday() % 7 + 6
-    nth = int(this_saturday / 7) + 1
-    return nth
 
 
 class Cronpy:
     def __init__(self, cron: str, now: datetime = None):
         """ Cron Expression:
+        REF: https://crontab.guru
         Minute; Hour; Day(of month); Month; Day(of week)
         0 3 * * * D-2 ==> 3AM everyday
         0 3 * * 2 W-1 ==> 3AM on every Tuesday
@@ -66,71 +47,64 @@ class Cronpy:
             self.cron_nweek = dow[dow.index('#')+1:]
         else:
             raise NotImplementedError(f'NOT SUPPORTED Day of Week: {dow}')
-        target = parts[5] if len(parts) >= 6 else 'D-0'
+        # target = parts[5] if len(parts) >= 6 else 'D-0'
         # METRIC RELATED-->
-        self.granularity = {'D': 'daily', 'W': 'weekly', 'M': 'monthly'}[target[0]]
-        delta_granularity = {'D': 'days', 'W': 'weeks', 'M': 'months'}[target[0]]
-        self.delta_n_periods = int(target[1:])
-        self.date_delta = relativedelta(**{delta_granularity: self.delta_n_periods})
+        # self.granularity = {'D': 'daily', 'W': 'weekly', 'M': 'monthly'}[target[0]]
+        # delta_granularity = {'D': 'days', 'W': 'weeks', 'M': 'months'}[target[0]]
+        # self.delta_n_periods = int(target[1:])
+        # self.date_delta = relativedelta(**{delta_granularity: self.delta_n_periods})
         # FINAL
         self.options = [[]] * 7  # HARD LIMIT
         self.fixed = [None] * 7
         # Set Initial Options (can be changed later)
         self._set_init_options(self.cron_minute, MINUTE, range(60))
-        self._set_init_options(self.cron_hour, MINUTE, range(60))
-        self._set_init_options(self.cron_dom, MINUTE, range(60))
-        self._set_init_options(self.cron_dow, MINUTE, range(60))
-        self._set_init_options(self.cron_nweek, MINUTE, range(60))
-        self._set_init_options(self.cron_month, MINUTE, range(60))
+        self._set_init_options(self.cron_hour, HOUR, range(24))
+        self._set_init_options(self.cron_month, MONTH, range(1, 13))
         self._set_init_options('*', YEAR, range(self.now.year, 2100))
+        self._set_day_options(self.now)
 
-    def _set_init_options(self, cron: str, idx: int, default_options: list) -> list:
-        default_options = sorted(default_options)
+    def _set_init_options(self, cron: str, idx: int, xrange: list):
+        options = self._get_options(cron, xrange)
+        self.options[idx] = options
+        if len(options) == 1:
+            self.fixed[idx] = options[0]
+        elif len(options) == 0:
+            raise NotImplementedError(f'NOT SUPPORTED {UNIT_NAMES[idx]}: {cron}')
+        return options
+
+    def _set_day_options(self, dt: datetime) -> bool:
+        mo_end_day = get_n_days_of_month(dt.year, dt.month)
+        max_weeks = how_many_weeks_of_month(dt.year, dt.month)
+        dom_options = self._get_options(self.cron_dom, range(1, mo_end_day + 1))
+        dow_options = self._get_options(self.cron_dow, range(1, 8))
+        nweek_options = self._get_options(self.cron_nweek, range(1, max_weeks + 1))
+        options = []
+        for dom in dom_options:
+            next_dt = dt.replace(day=dom)
+            dow = next_dt.isoweekday()
+            nth = get_nth_week_by_datetime(next_dt)
+            if dow in dow_options and nth in nweek_options:
+                options.append(next_dt.day)
+        self.options[DOM] = options
+        return True
+
+    def _get_options(self, cron: str, xrange: range) -> list:
+        options = []
         if is_int(cron):
-            self.options[idx] = [int(cron)]
-            self.fixed[idx] = int(cron)
+            options = [int(cron)]
         elif cron == '*':
-            self.options[idx] = default_options
+            options = xrange
         elif '/' in cron:
             every = int(cron[2:])
-            self.options[idx] = [i for i in default_options if i % every == 0]
+            options = [i for i in xrange if i % every == 0]
         elif ',' in cron:
-            self.options[idx] = sorted([int(s) % len(default_options) for s in cron.split(',')])
+            options = [int(s) for s in cron.split(',')]
         elif '-' in cron:
             start, end = cron.split('-')
-            self.options[idx] = range(int(start), int(end) + 1)
-        else:
-            raise NotImplementedError(f'NOT SUPPORTED {UNIT_NAMES[idx]}: {cron}')
-
-    # def _increase(self, dt: datetime) -> datetime:
-    #     # 1. INCREASE STARTS FROM THE LOWEST VARIANT POSITION
-    #     # 2. WILL CARRY TO THE NEXT HIGHER VARIANT POSITION IF APPLICABLE
-    #     dmn = (is_int(self.fixed[DOM]), is_int(self.fixed[DOW]), is_int(self.fixed[NWEEK]))
-    #     if self.fixed[MINUTE] is None:
-    #         next_dt = self._incr_minute(dt)
-    #     elif self.fixed[HOUR] is None:
-    #         next_dt = self._incr_hour(dt)
-    #     elif dmn == (False, False, False):  # -> * * *
-    #         next_dt = self._incr_dom(dt.replace(day=1))
-    #     elif dmn == (False, True, False):  # -> * 2 *
-    #         next_dt = self._incr_nweek(dt)
-    #     elif dmn == (False, True, True):  # -> * 2 #1
-    #         next_dt = self._incr_dom(dt)
-    #     elif dmn == (True, False, False):  # -> 5 * *
-    #         next_dt = self._incr_month(dt.replace(day=self.fixed[DOM]))
-    #     elif dmn == (True, True, False):  # -> 5 2 *
-    #         next_dt = self._incr_month(dt.replace(day=1))
-    #     elif dmn == (True, True, True):  # -> 5 2 #1
-    #         next_dt = self._incr_year(dt.replace(day=self.fixed[DOM]))
-    #     elif not self.fixed[NWEEK]:
-    #         next_dt = self._incr_nweek(dt)
-    #     elif not self.fixed[MONTH]:
-    #         next_dt = self._incr_month(dt)
-    #     elif not self.fixed[YEAR]:
-    #         next_dt = self._incr_year(dt)
-    #     else:
-    #         next_dt = dt
-    #     return next_dt
+            options = range(int(start), int(end) + 1)
+        carry_point = max(xrange) + 1
+        options = sorted({i % carry_point for i in options if i in xrange})
+        return options
 
     def _incr_year(self, dt: datetime) -> datetime:
         __import__('pudb').set_trace()
@@ -139,7 +113,6 @@ class Cronpy:
         return next_dt
 
     def _incr_month(self, dt: datetime) -> datetime:
-        __import__('pudb').set_trace()
         if self.fixed[MONTH]:
             return dt.replace(month=self.fixed[MONTH])
         options_next = [v for v in self.options[MONTH] if v > dt.month]
@@ -149,42 +122,22 @@ class Cronpy:
             next_dt = dt.replace(month=self.options[MONTH][0])
             next_dt = self._incr_year(next_dt)
         # UPDATE DAY OPTIONS WHEN MONTH/YEAR CHANGES
-        self.options[DOM] = list(range(1, YYMM_TO_END_DAY[(dt.year, dt.month)] + 1))
-        return next_dt
-
-    def _incr_nweek(self, dt: datetime) -> datetime:
-        __import__('pudb').set_trace()
-        if self.fixed[NWEEK]:
-            # return dt.replace(month=self.fixed[MONTH])
-            return dt
-        if dt.day + 7 > YYMM_TO_END_DAY([dt.year, dt.month]):
-            next_dt = self._incr_month(dt)
-        else:
-            nweek = get_nth_week_by_datetime(dt)
-            day = get_dom_by_nth_dow(dt.year, dt.month, dt.isoweekday(), nweek + 1)
-            next_dt = dt.replace(day=day)
-        return next_dt
-
-    def _incr_dow(self, dt: datetime) -> datetime:
-        __import__('pudb').set_trace()
-        if self.fixed[DOW]:
-            # return dt.replace(month=self.fixed[MONTH])
-            return dt
-
-    def _incr_dom(self, dt: datetime) -> datetime:
-        __import__('pudb').set_trace()
-        if self.fixed[DOM]:
-            return dt.replace(day=self.fixed[DOM])
-        end_day = YYMM_TO_END_DAY[(dt.year, dt.month)]
-        if dt.day != end_day:
-            next_dt = dt.replace(day=dt.day + 1)
-        else:
-            next_dt = self._incr_month(dt.replace(day=1))
+        self._set_day_options(next_dt)
+        if not self.options[DOM]:
+            self._incr_month(next_dt)
         return next_dt
 
     def _incr_day(self, dt: datetime) -> datetime:
-        # D M#N:
-        pass
+        __import__('pudb').set_trace()
+        # if all([self.fixed[DOM], self.fixed[DOW], self.fixed[NWEEK]]):
+        #     return dt
+        options_next = [v for v in self.options[DOM] if v > dt.day]
+        if options_next:
+            next_dt = dt.replace(day=options_next[0])
+        else:
+            next_dt = self._incr_month(dt)
+            next_dt = next_dt.replace(day=self.options[DOM][0])
+        return next_dt
 
     def _incr_hour(self, dt: datetime) -> datetime:
         __import__('pudb').set_trace()
@@ -196,7 +149,7 @@ class Cronpy:
         else:
             next_dt = dt.replace(hour=self.options[HOUR][0])
             # FIXME: NEED TO CHECK -> INCREASE ONLY WHEN Y/M/D IS NOT CERTAIN
-            next_dt = self._incr_dom(next_dt)
+            next_dt = self._incr_day(next_dt)
         return next_dt
 
     def _incr_minute(self, dt: datetime) -> datetime:
@@ -223,19 +176,13 @@ class Cronpy:
         )
         while dt.year < 2100:
             # CHECK
-            nweek = get_nth_week_by_datetime(dt)
-            dow = dt.isoweekday()
             checks = [
                 dt >= self.now,
                 dt.year in self.options[YEAR],
                 dt.month in self.options[MONTH],
                 dt.day in self.options[DOM],
-                dow in self.options[DOW],
-                nweek in self.options[NWEEK],
                 dt.hour in self.options[HOUR],
                 dt.minute in self.options[MINUTE],
-                # TODO: Special check of DMN-->
-                # ...
             ]
             __import__('pudb').set_trace()
             if all(checks):
--- cronpy/datetime_utils.py
+++ cronpy/datetime_utils.py
@@ -1,5 +1,4 @@
 from datetime import datetime, timedelta
-from dateutil.relativedelta import relativedelta
 
 
 def is_int(s: str) -> bool:
@@ -36,65 +35,25 @@ def date_to_time(date: datetime) -> str:
     return date.strftime('%Y-%m-%d %H:%M:%S')
 
 
-def date_to_start_end(dstr: str, granularity: str = 'daily') -> tuple:
-    assert isinstance(dstr, str) and isinstance(granularity, str)
-    start = end = day = datetime.strptime(dstr, '%Y-%m-%d')
-    if granularity == 'weekly':
-        weekday_n = day.isoweekday() % 7
-        start = day - timedelta(days=weekday_n)
-        end = day + timedelta(days=6 - weekday_n)
-    elif granularity == 'monthly':
-        start = day.replace(day=1)
-        end = (start + relativedelta(months=1)) - relativedelta(days=1)
-    start_str, end_str = start.strftime('%Y-%m-%d'), end.strftime('%Y-%m-%d')
-    return start_str, end_str
-
-
-def enumerate_dates(start_date: str, end_date: str, granularity: str) -> list:
-    start, end = datetime.strptime(start_date, '%Y-%m-%d'), datetime.strptime(end_date, '%Y-%m-%d')
-    daily_dates = [start + timedelta(days=i) for i in range((end - start).days + 1)]
-
-    target_dates = []
-    if granularity == 'daily':
-        target_dates = daily_dates
-    elif granularity == 'weekly':
-        # Start from Monday: weeday_index = day.weekday()
-        # Start from Sunday: weeday_index = (day.weekday() + 1) % 7
-        target_dates = sorted(set([day - timedelta(days=(day.weekday() + 1) % 7) for day in daily_dates]))  # Sundays
-    elif granularity == 'monthly':
-        target_dates = sorted(set([day.replace(day=1) for day in daily_dates]))
-
-    combos = [date_to_start_end(day.strftime('%Y-%m-%d'), granularity) for day in target_dates]
-    return combos
-
-
-def get_date_combinations(date_expression: str, granularity: str) -> list:
-    date_combos = []
-    if not date_expression:
-        return []
-    for dstr in str(date_expression).split(','):
-        if '_' in dstr:
-            start, end = dstr.split('_')
-            date_combos += enumerate_dates(start, end, granularity)
-        else:
-            start, end = date_to_start_end(dstr, granularity)
-            date_combos += [(start, end)]
-    return date_combos
-
-
 def get_n_days_of_month(year: int, month: int):
     dt = datetime(year=year, month=month % 12 + 1, day=1)
     n_days = (dt - timedelta(days=1)).day
     return n_days
 
 
-def get_dom_by_nth_weekday(year: str, month: str, weekday: int, nth: int) -> int:
-    assert nth <= 5
-    d = datetime(year=year, month=month, day=1)
-    weekdays = []
-    while d.month == month:
-        if d.isoweekday() == weekday % 7:
-            weekdays.append(d.day)
-        d += relativedelta(days=1)
-    dom = weekdays[nth - 1] if len(weekdays) >= nth else None
-    return dom
+def get_nth_week_by_datetime(dt: datetime) -> int:
+    head = 7 - datetime(year=dt.year, month=dt.month, day=1).isoweekday()
+    n = int((dt.day + 6 - dt.isoweekday() % 7 - head) / 7) + 1
+    return n
+
+
+def how_many_weeks_of_month(year: int, month: int) -> int:
+    max_days = get_n_days_of_month(year, month)
+    head = 7 - datetime(year=year, month=month, day=1).isoweekday()
+    tail = max_days - head - 28
+    n = 4
+    if head > 0:
+        n += 1
+    if tail > 0:
+        n += 1
+    return n
